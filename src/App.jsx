7b4489a3313ce14e198d94508ad25971f0f5c267
AcirDeveloper <acirdeveloper@gmail.com>
2022-12-23 00:04:06 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Canvas } from '@react-three/fiber'
+import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { Ground } from './components/Ground.jsx'
+import { FPV as Fpv } from './components/FPV.jsx'
+
+function App() {
+    return (
+        <Canvas>
+            <Sky sunPosition={[20, 20, 20]} />
+            <ambientLight intensity={0.5} />
+            <Fpv />
+            <Physics>
+                <Ground />
+            </Physics>
+        </Canvas>
+    )
+}
+
+export default App
